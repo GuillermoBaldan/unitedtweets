03ddd9a70d95c2503235fa588fc89759ca21cebe
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-17 20:53:19 +0200

    model user created

---
+++ api/models/user.js
@@ -0,0 +1,34 @@
+"use strict";
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: false,
+  },
+  nick: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "USER_ROLE",
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+});
