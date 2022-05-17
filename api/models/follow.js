e09695c6e22562e35410386d14f284a8ddaa8da7
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-17 21:12:11 +0200

    creation of follow data model

---
+++ api/models/follow.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const FollowSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  followed: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Follow", FollowSchema);
