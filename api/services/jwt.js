94c3ba0450a93922d7de33098fec5f09fabd68f7
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-18 11:54:12 +0200

    cofig token file added

---
+++ api/services/jwt.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const jwt = require("jwt-simple");
+const moment = require("moment");
+
+const secret = require("../config/config");
+
+exports.createToken = function (user) {
+  const payload = {
+    sub: user._id,
+    name: user.name,
+    surname: user.surname,
+    nick: user.nick,
+    email: user.email,
+    role: user.role,
+    image: user.image,
+    iat: moment().unix(),
+    exp: moment().add(30, "days").unix,
+  };
+
+  return jwt.encode(payload, secret.secretKey);
+};
