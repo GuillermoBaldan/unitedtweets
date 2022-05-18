7d7e0e432a4877ecc13513f2c55b43334b3e0c9c
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-18 17:57:12 +0200

    getUsers function implemented!

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -119,10 +119,36 @@ function getUser(req, res) {
   });
 }
 
+function getUsers(req, res) {
+  const identity_user_id = req.user.sub;
+
+  let page = 1;
+  if (req.params.page) {
+    page = req.params.page;
+  }
+  const itemsPerPage = 5;
+
+  User.find()
+    .sort("_id")
+    .paginate(page, itemsPerPage, (err, users, total) => {
+      if (err) return res.status(500).send({ message: "Error en la petición" });
+
+      if (!users)
+        return res.status(404).send({ message: "No hay usuarios disponibles" });
+
+      return res.status(200).send({
+        users,
+        total,
+        pages: Math.ceil(total / itemsPerPage),
+      });
+    });
+}
+
 module.exports = {
   home,
   pruebas,
   saveUser,
   loginUser,
   getUser,
+  getUsers,
 };
