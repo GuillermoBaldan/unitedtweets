00154e2cb2d117905d71d98e6efeb0965599bfa6
GuillermoBaldan <guillermobaldan@gmail.com>
2022-05-18 11:23:59 +0200

    route /register added to routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,5 +6,6 @@ const api = express.Router();
 
 api.get("/home", UserController.home);
 api.get("/pruebas", UserController.pruebas);
+api.post("/register", UserController.saveUser);
 
 module.exports = api;
